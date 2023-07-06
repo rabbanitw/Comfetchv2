ca70799c2f57c2f0d3b598c2142ff54f266833d9
Marcob1996 <marcob@umd.edu>
2023-07-06 17:04:20 -0400

    Add in pruning tests

--- prune-0.5.sh
+++ prune-0.5.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=prune0.5   # sets the job name if not set from environment
 #SBATCH --time=10:00:00    # how long you think your job will take to complete; format=hh:mm:ss
-#SBATCH --account=furongh    # set QOS, this will determine what resources can be requested
-#SBATCH --qos=high    # set QOS, this will determine what resources can be requested
-#SBATCH --partition=dpart
+#SBATCH --account=scavenger    # set QOS, this will determine what resources can be requested
+#SBATCH --qos=scavenger    # set QOS, this will determine what resources can be requested
+#SBATCH --partition=scavenger
 #SBATCH --ntasks=4
 #SBATCH --mem 32gb         # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --gres=gpu:2
 #SBATCH --nice=0
 #SBATCH --mail-type=END   # Valid type values are NONE, BEGIN, END, FAIL, REQUEUE
-#SBATCH --nodelist=cml21
 
 module purge
 module load mpi
